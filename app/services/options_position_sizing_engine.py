b150bac94505534c0092703219411c81680afb68
Thomas Palenske <thomaspalenske@gmail.com>
2026-06-24 11:35:26 -0500

    Skip option contracts above position size cap

--- app/services/options_position_sizing_engine.py
+++ app/services/options_position_sizing_engine.py
@@ -12,8 +12,10 @@ class OptionsPositionSizingEngine:
 
         if contract_cost <= 0:
             contracts = 0
+        elif contract_cost > max_position_dollars:
+            contracts = 0
         else:
-            contracts = max(int(max_position_dollars // contract_cost), 1)
+            contracts = int(max_position_dollars // contract_cost)
 
         return {
             "account_equity": equity,
@@ -22,6 +24,7 @@ class OptionsPositionSizingEngine:
             "option_ask": ask,
             "contract_cost": round(contract_cost, 2),
             "recommended_contracts": contracts,
+            "sizing_action": "SKIP_CONTRACT_TOO_EXPENSIVE" if contracts == 0 and contract_cost > max_position_dollars else "APPROVED",
             "estimated_position_cost": round(contracts * contract_cost, 2),
             "status": "OPTIONS_POSITION_SIZE_READY",
         }
