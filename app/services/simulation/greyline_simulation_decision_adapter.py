ad350e80c738df9ef0673f84dc937327230d8143
Thomas Palenske <thomaspalenske@gmail.com>
2026-07-05 20:52:18 -0500

    Block weak historical XLE call simulations

--- app/services/simulation/greyline_simulation_decision_adapter.py
+++ app/services/simulation/greyline_simulation_decision_adapter.py
@@ -178,6 +178,8 @@ class GreyLineSimulationDecisionAdapter:
             execution_blockers.append("INSTITUTIONAL_SPONSORSHIP_BELOW_80")
         if option_type == "PUT" and bear_trend_score >= 84 and bear_setup_score >= 90:
             execution_blockers.append("PUT_DOWNSIDE_EXHAUSTION_RISK")
+        if symbol == "XLE" and option_type == "CALL":
+            execution_blockers.append("XLE_CALL_HISTORICAL_WEAKNESS_BLOCK")
         call_risk_ok = not (option_type == "CALL" and risk_state_result.get("risk_state_score", 50) < 80)
         call_bear_rally_ok = not (
             option_type == "CALL"
